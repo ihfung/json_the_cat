6efed6b29f5afc61fca33f8a469c7ca1932afd33
ihleung <ihfungleung@gmail.com>
2024-05-09 13:05:26 -0400

    updated

--- breedFetch.js
+++ breedFetch.js
@@ -1,1 +1,11 @@
 const needle = require('needle');
+
+
+needle.get('https://api.thecatapi.com/v1/breeds/search?q=Siberian', (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    
+  }
+  console.log('body:', body); // Print the HTML for the Google homepage.
+  console.log(typeof body);
+});
